fdfb6efbc680fef5a18963d40818d3f6b0d194d2
MU JIANG <1819021@qq.com>
2016-03-28 14:01:02 +0800

    initial opextools project

---
+++ src/main/java/com/expedia/lodging/opextools/OpextoolsApplication.java
@@ -0,0 +1,12 @@
+package com.expedia.lodging.opextools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpextoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpextoolsApplication.class, args);
+	}
+}
